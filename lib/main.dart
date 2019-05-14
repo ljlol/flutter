a6c6ad9cc1a734301063a6c7b9c39e75f84cf6fc
ljlol <mosaki.mike@gmail.com>
2019-05-14 15:51:08 +0800

    边距 背景 边框

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,11 +9,26 @@ class MyApp extends StatelessWidget{
         return MaterialApp(
             title: '欢迎玩flutter',
             home: Scaffold(
-                appBar: AppBar(
-                    title: Text('欢迎来玩flutter'),
-                ),
+                
                 body: Center(
-                    child: Text('hello world'),
+                    child: Container(
+                        child: new Text(
+                            'hello container',
+                            style: TextStyle(fontSize: 40.0),
+                        ),
+                        alignment: Alignment.topLeft,
+                        // color: Colors.lightBlue,
+                        width: 500.0,
+                        height: 400.0,
+                        padding: EdgeInsets.all(10.0),
+                        margin: EdgeInsets.all(5.0),
+                        decoration: new BoxDecoration(
+                            gradient: const LinearGradient(
+                                colors: [Colors.lightBlue,Colors.greenAccent,Colors.purple]
+                            ),
+                            border: Border.all(width: 2.0,color: Colors.red)
+                        ),
+                    )
                 ),
             ),
         );
