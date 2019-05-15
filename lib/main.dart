0a5c217c16685ec4daac5f45d57df8ca6c2b4cda
ljlol <mosaki.mike@gmail.com>
2019-05-15 13:59:54 +0800

    横向列表以及组件拆分

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,41 @@ class MyApp extends StatelessWidget{
                 appBar: new AppBar(
                     title: new Text('listview widget'),
                 ),
-                body: new ListView(
-                    children: <Widget>[
-                    
-                        new Image.network(
-                            'https://www.baidu.com/img/bd_logo1.png?where=super'
-                        ),
-                        new Image.network(
-                            'https://www.baidu.com/img/bd_logo1.png?where=super'
-                        ),
-                        new Image.network(
-                            'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=57280421,1165147730&fm=85&app=63&f=JPEG?w=121&h=75&s=F4FEAD7757A34715CC5E596D0300306A'
-                        ),
-                    ],
-                ),
+                body: Center(
+                    child: Container(
+                        height: 200.0,
+                        child: MyList(),
+                    ),
+                )
             ),
+        );
+    }
+}
+
+class MyList extends StatelessWidget{
+    @override
+    Widget build(BuildContext context) {
+    // TODO: implement build
+        return ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+                new Container(
+                    width: 180.0,
+                    color: Colors.lightBlue,
+                ),
+                new Container(
+                    width: 180.0,
+                    color: Colors.amber,
+                ),
+                new Container(
+                    width: 180.0,
+                    color: Colors.deepOrange,
+                ),
+                new Container(
+                    width: 180.0,
+                    color: Colors.deepPurpleAccent,
+                ),
+            ]
         );
     }
 }
