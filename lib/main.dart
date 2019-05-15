60bc98daca070d3adb39daae1338b96fded33225
ljlol <mosaki.mike@gmail.com>
2019-05-15 17:09:06 +0800

    导航跳转

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-void main() => runApp(MyApp(
-    
+void main() => runApp(MaterialApp(
+    title: "导航1",
+    home: FirstScreen(),
 ));
 
-class MyApp extends StatelessWidget{
-
-    
-
-    @override
-    Widget build(BuildContext context) {
-        var stack = new Stack(
-            alignment: const FractionalOffset(0.5, 0.5),
-            children: <Widget>[
-                new CircleAvatar(
-                    backgroundImage: new NetworkImage('https://www.baidu.com/img/bd_logo1.png?where=super'),
-                    radius: 100.0,
-                ),
-                new Positioned(
-                    top: 10.0,
-                    left: 10.0,
-                    child: Text('www.test.com'),
-                ),
-                new Positioned(
-                    bottom: 10.0,
-                    right: 10.0,
-                    child: Text('六六六'),
-                )
-            ],
-        );
-
-        var card = new Card(
-            child: Column(
-                children: <Widget>[
-                    ListTile(
-                        title: Text('杭州市滨江区',style:TextStyle(fontWeight: FontWeight.w500)),
-                        subtitle: Text('666：13322332233'),
-                        leading: Icon(Icons.access_time,color:Colors.lightBlue),
-                    ),
-                    new Divider(),
-                    ListTile(
-                        title:new Text('北京市海淀区中国科技大学',style: TextStyle(fontWeight: FontWeight.w500),),
-                        subtitle: new Text('2222:1513938888'),
-                        leading: new Icon(Icons.account_box,color: Colors.lightBlue,),
-                    ),
-                    new Divider(),
-                    ListTile(
-                        title: Text('浙江省绍兴市越城区',style: TextStyle(fontWeight: FontWeight.w400),),
-                        subtitle: Text('777:122233333'),
-                        leading: Icon(Icons.account_balance,color:Colors.deepOrange),
-                    ),
-                    new Divider()
-                ],
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("导航页面"),
+            
+        ),
+        body: Center(
+            child: RaisedButton(
+                child: Text("查看详情"),
+                onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(
+                        builder:(context)=> new SecondScreen()
+                    ));
+                   
+                },
             ),
-        );
-        return MaterialApp(
-            title: '欢迎玩flutter',
-            home: Scaffold(
-                appBar: new AppBar(
-                    title: new Text('stack'),
-                ),
-                body: Center(
-                    child: card,
-                )
-            ),
-        );
-    }
+        ),
+    );
+  }
 }
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('第二页面'),
+      ),
+      body: Center(
+          child: RaisedButton(
+              child: Text('返回'),
+              onPressed: (){
+                  Navigator.pop(context);
+              },
+          ),
+          
+      ),
+    );
+  }
+}
+
