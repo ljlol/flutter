8279475fbf9360a9efab260ec6c46db6a43cbedf
ljlol <mosaki.mike@gmail.com>
2019-05-15 13:36:10 +0800

    listview组件

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,22 @@ class MyApp extends StatelessWidget{
         return MaterialApp(
             title: '欢迎玩flutter',
             home: Scaffold(
-                
-                body: Center(
-                    child: Container(
-                        child: new Image.network(
-                            'https://www.baidu.com/img/bd_logo1.png?where=super',
-                            scale: 1.0,
-                            color: Colors.greenAccent,
-                            colorBlendMode: BlendMode.darken,
+                appBar: new AppBar(
+                    title: new Text('listview widget'),
+                ),
+                body: new ListView(
+                    children: <Widget>[
+                    
+                        new Image.network(
+                            'https://www.baidu.com/img/bd_logo1.png?where=super'
                         ),
-                        width: 300.0,
-                        height: 200.0,
-                        color: Colors.lightBlue,
-                    )
+                        new Image.network(
+                            'https://www.baidu.com/img/bd_logo1.png?where=super'
+                        ),
+                        new Image.network(
+                            'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=57280421,1165147730&fm=85&app=63&f=JPEG?w=121&h=75&s=F4FEAD7757A34715CC5E596D0300306A'
+                        ),
+                    ],
                 ),
             ),
         );
