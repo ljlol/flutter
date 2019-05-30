c840838f26ef9eb6a3acda12d147a6777d25ca13
ljlol <mosaki.mike@gmail.com>
2019-05-30 17:36:08 +0800

    布局学习

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,125 @@ import 'package:english_words/english_words.dart';
 import 'package:testapp/app.dart';
 
 void main(List<String> args) {
-    runApp(ShrineApp());
+    runApp(UiApp());
+}
+
+class UiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    var packedRow = new Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        new Icon(Icons.star, color: Colors.green[500]),
+        new Icon(Icons.star, color: Colors.green[500]),
+        new Icon(Icons.star, color: Colors.green[500]),
+        new Icon(Icons.star, color: Colors.black),
+        new Icon(Icons.star, color: Colors.black),
+      ],
+    );
+
+    var ratings = new Container(
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+            ],
+          ),
+          Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily:'Roboro',
+              letterSpacing:0.5,
+              fontSize: 20.0,
+            )
+          )
+
+        ],
+      ),
+    );
+
+    var descTextStyle = new TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18.0,
+      height: 2.0,
+    );
+
+    // DefaultTextStyle.merge可以允许您创建一个默认的文本样式，该样式会被其
+    // 所有的子节点继承
+    var iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: new Container(
+        padding: new EdgeInsets.all(20.0),
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            new Column(
+              children: [
+                new Icon(Icons.kitchen, color: Colors.green[500]),
+                new Text('PREP:'),
+                new Text('25 min'),
+              ],
+            ),
+            new Column(
+              children: [
+                new Icon(Icons.timer, color: Colors.green[500]),
+                new Text('COOK:'),
+                new Text('1 hr'),
+              ],
+            ),
+            new Column(
+              children: [
+                new Icon(Icons.restaurant, color: Colors.green[500]),
+                new Text('FEEDS:'),
+                new Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+
+    var leftColumn = Container(
+      padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
+      child: Column(
+        children: <Widget>[
+          ratings,
+          iconList
+
+        ],
+      ),
+    );
+
+    return MaterialApp(
+      
+    
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('123344'),
+        ),
+
+        body: Center(
+          child: leftColumn,
+        ),
+      )
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
